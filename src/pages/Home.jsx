fd4088c8f85aa3b4e7e5fd9fc4c8da621498067f
Odd Egil Hegge Selnes <post@odde.org>
2025-05-18 17:48:44 +0200

    added google fonts from style guide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-      <div class="bg-coral text-sand">Welcome to Holidaze 🌴</div>
+      <div className="bg-coral text-sand">
+        <h1 className="text-4xl font-[dm_sans] text-transform-uppercase font-bold">
+          WELCOME TO HOLIDAZE 🌴 PRIMARY FONT (HEADLINES, BRANDING)
+        </h1>
+      </div>
 
-      <button class="bg-amber-dark hover:bg-amber text-white px-4 py-2 rounded">
+      <button className="bg-amber-dark hover:bg-amber text-white px-4 py-2 rounded">
         Book Now
       </button>
     </>
